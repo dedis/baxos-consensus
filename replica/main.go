95667743906111e6d50f39ab3e28a650a674fae0
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-08-02 21:24:07 +0200

    done replica/main.go

--- replica/main.go
+++ replica/main.go
@@ -10,18 +10,17 @@ import (
 
 func main() {
 	name := flag.Int64("name", 1, "name of the replica as specified in the local-configuration.yml")
-	configFile := flag.String("config", "configuration/local/local-configuration.yml", "configuration file")
+	configFile := flag.String("config", "configuration/local-configuration.yml", "configuration file")
 	logFilePath := flag.String("logFilePath", "logs/", "log file path")
 	batchSize := flag.Int("batchSize", 50, "batch size")
 	batchTime := flag.Int("batchTime", 5000, "maximum time to wait for collecting a batch of requests in micro seconds")
 	debugOn := flag.Bool("debugOn", false, "false or true")
 	isAsync := flag.Bool("isAsync", false, "false or true to simulate asynchrony")
 	debugLevel := flag.Int("debugLevel", 0, "debug level")
-	viewTimeout := flag.Int("viewTimeout", 2000000, "view timeout in micro seconds")
+	roundTripTime := flag.Int("roundTripTime", 2000, "round trip time in micro seconds")
 	keyLen := flag.Int("keyLen", 8, "key length")
 	valLen := flag.Int("valLen", 8, "value length")
 	benchmarkMode := flag.Int("benchmarkMode", 0, "0: resident store, 1: redis")
-	pipelineLength := flag.Int("pipelineLength", 1, "pipeline length")
 	asyncTimeout := flag.Int("asyncTimeout", 500, "artificial asynchronous timeout in milli seconds")
 	timeEpochSize := flag.Int("timeEpochSize", 500, "duration of a time epoch for the attacker in milli seconds")
 
@@ -33,7 +32,7 @@ func main() {
 		panic(err)
 	}
 
-	rp := src.New(int32(*name), cfg, *logFilePath, *batchSize, *batchTime, *debugOn, *debugLevel, *viewTimeout, *benchmarkMode, *keyLen, *valLen, *pipelineLength, *isAsync, *asyncTimeout, *timeEpochSize)
+	rp := src.New(int32(*name), cfg, *logFilePath, *batchSize, *batchTime, *debugOn, *debugLevel, *benchmarkMode, *keyLen, *valLen, *isAsync, *asyncTimeout, *timeEpochSize, int64(*roundTripTime))
 
 	rp.WaitForConnections()
 	rp.StartOutgoingLinks()
