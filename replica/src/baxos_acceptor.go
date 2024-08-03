e46bb4991a72e520733aec37b85905d418ab919b
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-08-03 18:00:19 +0200

    checked baxos_acceptor.go

--- replica/src/baxos_acceptor.go
+++ replica/src/baxos_acceptor.go
@@ -1,6 +1,9 @@
 package src
 
-import "baxos/common"
+import (
+	"baxos/common"
+	"fmt"
+)
 
 /*
 	Logic for prepare message, check if it is possible to promise for the specified instance
@@ -10,6 +13,9 @@ func (rp *Replica) processPrepare(message *common.PrepareRequest) *common.Promis
 	rp.createInstance(int(message.InstanceNumber))
 
 	if rp.baxosConsensus.replicatedLog[message.InstanceNumber].decided {
+		if rp.debugOn {
+			rp.debug(fmt.Sprintf("Instance %d already decided, hence sending a promise reply with the decided value", message.InstanceNumber), 0)
+		}
 		return &common.PromiseReply{
 			InstanceNumber: message.InstanceNumber,
 			Promise:        false,
@@ -21,6 +27,9 @@ func (rp *Replica) processPrepare(message *common.PrepareRequest) *common.Promis
 
 	if rp.baxosConsensus.replicatedLog[message.InstanceNumber].acceptor_bookkeeping.promisedBallot < message.PrepareBallot {
 		rp.baxosConsensus.replicatedLog[message.InstanceNumber].acceptor_bookkeeping.promisedBallot = message.PrepareBallot
+		if rp.debugOn {
+			rp.debug(fmt.Sprintf("Prepare accepted, hence sending a promise reply", message.InstanceNumber), 0)
+		}
 		return &common.PromiseReply{
 			InstanceNumber:     message.InstanceNumber,
 			Promise:            true,
@@ -49,6 +58,9 @@ func (rp *Replica) handlePrepare(message *common.PrepareRequest) {
 		}
 
 		rp.sendMessage(int32(message.Sender), rpcPair)
+		if rp.debugOn {
+			rp.debug(fmt.Sprintf("Sent a promise response to %d for instance %d", message.Sender, message.InstanceNumber), 0)
+		}
 	}
 }
 
@@ -58,6 +70,9 @@ func (rp *Replica) processPropose(message *common.ProposeRequest) *common.Accept
 	rp.createInstance(int(message.InstanceNumber))
 
 	if rp.baxosConsensus.replicatedLog[message.InstanceNumber].decided {
+		if rp.debugOn {
+			rp.debug(fmt.Sprintf("Instance %d already decided, hence sending a accept reply with the decided value", message.InstanceNumber), 0)
+		}
 		return &common.AcceptReply{
 			InstanceNumber: message.InstanceNumber,
 			Accept:         false,
@@ -70,6 +85,9 @@ func (rp *Replica) processPropose(message *common.ProposeRequest) *common.Accept
 	if message.ProposeBallot >= rp.baxosConsensus.replicatedLog[message.InstanceNumber].acceptor_bookkeeping.promisedBallot {
 		rp.baxosConsensus.replicatedLog[message.InstanceNumber].acceptor_bookkeeping.acceptedBallot = message.ProposeBallot
 		rp.baxosConsensus.replicatedLog[message.InstanceNumber].acceptor_bookkeeping.acceptedValue = *message.ProposeValue
+		if rp.debugOn {
+			rp.debug(fmt.Sprintf("Accepted propose for instance %d, hence sending a accept", message.InstanceNumber), 0)
+		}
 		return &common.AcceptReply{
 			InstanceNumber: message.InstanceNumber,
 			Accept:         true,
@@ -77,6 +95,9 @@ func (rp *Replica) processPropose(message *common.ProposeRequest) *common.Accept
 			Sender:         int64(rp.name),
 		}
 	} else {
+		if rp.debugOn {
+			rp.debug(fmt.Sprintf("Propose rejected for instance %d", message.InstanceNumber), 0)
+		}
 		return &common.AcceptReply{
 			InstanceNumber: message.InstanceNumber,
 			Accept:         false,
@@ -103,11 +124,14 @@ func (rp *Replica) handlePropose(message *common.ProposeRequest) {
 		Code: rp.messageCodes.AcceptReply,
 		Obj:  acceptReply,
 	})
+	if rp.debugOn {
+		rp.debug(fmt.Sprintf("Sent accept message to %d for instance %d", message.Sender, message.InstanceNumber), 0)
+	}
 
 	// handle the decided slot
 	if message.DecideInfo != nil {
 		rp.createInstance(int(message.DecideInfo.InstanceNumber))
-		if rp.baxosConsensus.replicatedLog[message.DecideInfo.InstanceNumber].decided {
+		if !rp.baxosConsensus.replicatedLog[message.DecideInfo.InstanceNumber].decided {
 			rp.baxosConsensus.replicatedLog[message.DecideInfo.InstanceNumber].decided = true
 			rp.baxosConsensus.replicatedLog[message.DecideInfo.InstanceNumber].decidedValue = *message.DecideInfo.DecidedValue
 			rp.updateSMR()
